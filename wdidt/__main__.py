c03b5851041ed9c11ef600f671462d8f9eb432e1
Derek Keeler <derek@suchcool.ca>
2022-11-03 21:25:05 -0700

    Switch to pyproject.toml use - Update to python 3.11 - Create PR/CI workflow - Fix typing issues - Fix formatting warnings

--- wdidt/__main__.py
+++ wdidt/__main__.py
@@ -16,11 +16,11 @@ Options:
   -d --dry-run              Run without creating the new daily log. Report what would be written.
   -A --attributes <attrib>  Added attributes to render the daily log with.
 """
-
 import datetime
-import docopt
 import logging
 import sys
+
+import docopt
 
 from . import new_day
 
@@ -31,7 +31,9 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    opts = docopt.docopt(doc=__doc__, argv=sys.argv[1:], help=True, version="0.1")
+    opts = docopt.docopt(
+        doc=__doc__ if __doc__ else "", argv=sys.argv[1:], help=True, version="0.1"
+    )
 
     log_level = logging.INFO
     if opts.get("--verbose", False):
@@ -42,11 +44,16 @@ if __name__ == "__main__":
     log.debug("Arguments given:")
     log.debug(opts)
 
-    log_day = datetime.date.today()
+    log_day: datetime.date = datetime.date.today()
     if opts.get("--ago", None):
         log_day = datetime.date.today() - datetime.timedelta(
             days=int(opts.get("--ago", 0))
         )
         log.debug(f"Log file day is being set to {log_day}.")
-    
-    new_day.create_new_day(log_day, opts.get("--force", False), opts.get("--dry-run", False), opts.get("--attributes", None))
+
+    new_day.create_new_day(
+        log_day=log_day,
+        force=opts.get("--force", False),
+        dry_run=opts.get("--dry-run", False),
+        attribs=opts.get("--attributes", None),
+    )
