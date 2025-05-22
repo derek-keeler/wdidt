b640b140978ed4fec6cc578007c2ec1144971dc0
Derek Keeler <dekeeler@microsoft.com>
2025-05-22 15:47:15 -0700

    Replace docopt with Click

--- wdidt/__main__.py
+++ wdidt/__main__.py
@@ -1,26 +1,13 @@
 """new_day.py
 
 Program to create a new daily log file in the WDIDT format.
-
-Usage:
-  new_day.py [-a <num_days> | --ago=<num_days>] [-f | --force] [-V | --verbose] [-d | --dry-run] [-A <attrib> | --attributes=<attrib>]
-  new_day.py -h | --help
-  new_day.ph -v | --version
-
-Options:
-  -h --help                 Show this help and exit.
-  -v --version              Show the version of this script and exit.
-  -f --force                Always create the log file, even if it exists.
-  -V --verbose              Show verbose logs during processing.
-  -a --ago <num_days>       Create a log file for num_days ago.
-  -d --dry-run              Run without creating the new daily log. Report what would be written.
-  -A --attributes <attrib>  Added attributes to render the daily log with.
 """
+
 import datetime
 import logging
-import sys
 
-import docopt
+
+import click
 
 from . import new_day
 
@@ -30,30 +17,68 @@ logging.basicConfig(
 
 log = logging.getLogger(__name__)
 
-if __name__ == "__main__":
-    opts = docopt.docopt(
-        doc=__doc__ if __doc__ else "", argv=sys.argv[1:], help=True, version="0.1"
-    )
 
+@click.command()
+@click.option(
+    "-a",
+    "--ago",
+    type=int,
+    default=0,
+    help="Create a log file for num_days ago.",
+)
+@click.option(
+    "-f",
+    "--force",
+    is_flag=True,
+    help="Always create the log file, even if it exists.",
+)
+@click.option(
+    "-V",
+    "--verbose",
+    is_flag=True,
+    help="Show verbose logs during processing.",
+)
+@click.option(
+    "-d",
+    "--dry-run",
+    is_flag=True,
+    help="Run without creating the new daily log. Report what would be written.",
+)
+@click.option(
+    "-A",
+    "--attributes",
+    default=None,
+    help="Added attributes to render the daily log with.",
+)
+@click.version_option("0.1", "-v", "--version", message="%(version)s")
+def main(ago, force, verbose, dry_run, attributes):
+    """Program to create a new daily log file in the WDIDT format."""
     log_level = logging.INFO
-    if opts.get("--verbose", False):
+    if verbose:
         log_level = logging.DEBUG
-
     log.setLevel(log_level)
 
-    log.debug("Arguments given:")
-    log.debug(opts)
+    log.debug(
+        f"Arguments given:\n"
+        f"    ago={ago}\n"
+        f"    force={force}\n"
+        f"    verbose={verbose}\n"
+        f"    dry_run={dry_run}\n"
+        f"    attributes={attributes}"
+    )
 
     log_day: datetime.date = datetime.date.today()
-    if opts.get("--ago", None):
-        log_day = datetime.date.today() - datetime.timedelta(
-            days=int(opts.get("--ago", 0))
-        )
+    if ago:
+        log_day = datetime.date.today() - datetime.timedelta(days=ago)
         log.debug(f"Log file day is being set to {log_day}.")
 
     new_day.create_new_day(
         log_day=log_day,
-        force=opts.get("--force", False),
-        dry_run=opts.get("--dry-run", False),
-        attribs=opts.get("--attributes", None),
+        force=force,
+        dry_run=dry_run,
+        attribs=attributes,
     )
+
+
+if __name__ == "__main__":
+    main()
